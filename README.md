47ce414023eacae00eff6105e6ee5fe035dbcf31
Thomas Gardner <tpgardner19@gmail.com>
2016-12-11 19:53:27 -0600

    Added twitter querying script and section in README

--- README.md
+++ README.md
@@ -4,5 +4,18 @@
 
 ## Part One
 ![Santa's Business Card](./business_card.PNG)
+* Python script: `tweet_finder.py`
+  * Query a target Twitter user's account
+  * Print out all the statuses of that user
+* Configuration File: `config.json`
+```
+{
+    "target": <Username of Twitter Account to Query>,
+    "consumerKey": <API Key>,
+    "consumerSecret": <Secret Key>,
+    "accessToken": <Application API Key>,
+    "accessSecret": <Application Secret>
+}
+```
 1) What is the secrete message in Santa's tweets?
 2) What is inside the ZIP file distributed by Santa's team?
---
+++ tweet_finder.py
@@ -0,0 +1,29 @@
+"""Script to access a specific Twitter user's tweets"""
+import json
+from twypy.api import Api
+
+
+def read_config(filename):
+    """
+    Read a JSON file for info about the Twitter API
+    Input
+        filename: Path to the JSON config file
+    No Output
+    """
+    with open(filename, "r") as ifile:
+        return json.loads(ifile.read())
+
+
+if __name__ == '__main__':
+    config = read_config("config.json")
+    client = Api(
+        consumer_key=config["consumerKey"],
+        consumer_secret=config["consumerSecret"],
+        access_token=config["accessToken"],
+        access_token_secret=config["accessSecret"]
+    )
+    statuses = []
+    response = client.api.statuses.user_timeline.get(screen_name=config["target"])
+    [statuses.append(status["text"]) for status in response]
+    print("".join(statuses))
+    print(json.dumps(response[0], indent=4))
